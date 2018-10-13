6e5ac2f80817ed98203eee354f9ef219d9caf4bf
Chris <cachrissie@gmail.com>
2018-10-13 14:55:37 +0200

    jetpack geïmplementeerd

---
+++ Assets/Scripts/Actions/ChanneledAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+public class ChanneledAction : PolyJumperAction
+{
+	private bool channeling;
+	public PolyJumperAction actionToChannel;
+
+	public override void RpcDoAction()
+	{
+		channeling = !channeling;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (channeling && Restrictions.All(x => x.CanExecuteAction()))
+		{
+			actionToChannel.RpcDoAction();
+			Restrictions.Select(r =>
+			{
+				r.ActionWasExecuted();
+				return false;
+			}).Count();
+		}
+	}
+}
---
+++ Assets/Scripts/Actions/JetpackAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class JetpackAction : PolyJumperAction
+{
+	public Vector3 JumpVector;
+	
+	[ClientRpc]
+	public override void RpcDoAction()
+	{
+		var rigidbody = GetComponent<Rigidbody>();
+		rigidbody.AddForce(JumpVector - rigidbody.velocity, ForceMode.Acceleration);
+	}
+}
